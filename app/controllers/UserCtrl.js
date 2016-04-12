00cc01b4d029686ededed62439504d59c579ad90
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-04-12 18:29:43 +0200

    remove unused code

--- app/controllers/UserCtrl.js
+++ app/controllers/UserCtrl.js
@@ -253,39 +253,7 @@
 				}
             });
 
-
-			/*
-            $scope.user.$save().then(function(ret) {
-                console.log("user saved ", ret);
-				$scope.hide();
-            }, function(err) {
-                console.log("err ! ", err);
-				if (err.data.errors) {
-					arkeoService.setFormErrorsFromServer(userForm, err.data.errors, "json.");
-				}
-            });
-			*/
         };
-
-/*
-		$scope.uploadForm = function(url, json, file) {
-			file.upload = Upload.upload({
-				url: url,
-				data: {username: $scope.username, file: file},
-			});
-
-			file.upload.then(function (response) {
-				$timeout(function () {
-					file.result = response.data;
-				});
-			}, function (response) {
-				if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-			}, function (evt) {
-				// Math.min is to fix IE which reports 200% sometimes
-				file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-			});
-		}
-*/
 
 	}]);
 
