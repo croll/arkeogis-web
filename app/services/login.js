160f111a9eb1c05ad06f72fa057d8f20de596db0
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-06-07 18:31:32 +0200

    add relogin call in login service

--- app/services/login.js
+++ app/services/login.js
@@ -49,6 +49,21 @@
             return promise;
         };
 
+        this.relogin = function() {
+            var promise = $q(function(resolve, reject) {
+                $http.get('/api/relogin').then(function(ret) {
+                    self.user = new User(ret.data.User);
+                    arkeoLang.setUserLang(1, ret.data.lang1.iso_code)
+                    arkeoLang.setUserLang(2, ret.data.lang2.iso_code)
+                    self.permissions = ret.data.permissions;
+                    resolve(self.user);
+                }, function(err) {
+                    reject(err);
+                });
+            });
+            return promise;
+        };
+        this.relogin();
 
         /*
          * check if the user has the permission "permname"
