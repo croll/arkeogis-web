533bcb6da5c3881c6b86099be01b53f75dcb82d3
Christophe Beveraggi <beve@croll.fr>
2016-06-24 17:35:51 +0200

    Fix method getValidBoundingBox

--- app/services/map.js
+++ app/services/map.js
@@ -74,26 +74,28 @@
             }
         }
 
-        this.getValidBoundingBox = function(minx, miny, maxx, maxy) {
-            minx = parseFloat(minx);
-            miny = parseFloat(miny);
-            maxx = parseFloat(maxx);
-            maxy = parseFloat(maxy);
-            if (minx <= -90) {
-                minx = -90;
+        this.getValidBoundingBox = function(north, east, south, west) {
+            north = parseFloat(north);
+            east = parseFloat(east);
+            south = parseFloat(south);
+            west = parseFloat(west);
+            if (north < -90) {
+                north = -90;
             }
-            if (maxx >= 90) {
-                maxx = 89.999999;
+            if (south >= 90) {
+                south = 89.999999;
             }
-            if (miny <= -180) {
-                miny = -180;
+            if (west < -180) {
+                west = -180;
             }
-            if (maxy >= 180) {
-                maxy = 179.999999;
+            if (east >= 180) {
+                east = 179.999999;
             }
+            console.log([north, east],
+                [south, west]);
             return [
-                [minx, miny],
-                [maxx, maxy]
+                [north, east],
+                [south, west]
             ]
         }
 
