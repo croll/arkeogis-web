a7fa1002b07c579355985c118d05f357ace0583f
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-06-25 08:26:35 +0200

    remove some strange comments

--- app/controllers/CharacEditorCtrl.js
+++ app/controllers/CharacEditorCtrl.js
@@ -4,7 +4,7 @@
  * Authors :
  *  Nicolas Dimitrijevic <nicolas@croll.fr>
  *  Christophe Beveraggi <beve@croll.fr>
- *insert into group__permission values (1, 7, now(), now());chj
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
