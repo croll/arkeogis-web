95ab41b73a6b55c44dd72b9c70b2633246d2831d
Christophe Beveraggi <beve@croll.fr>
2016-06-25 12:30:07 +0200

    new function mapService.getBoundsAsGeoJSON() whiches solves area queries

--- app/services/map.js
+++ app/services/map.js
@@ -79,25 +79,28 @@
             east = parseFloat(east);
             south = parseFloat(south);
             west = parseFloat(west);
-            if (north < -90) {
-                north = -90;
+            if (north > 90) {
+                north = 89.999999;
             }
-            if (south >= 90) {
-                south = 89.999999;
+            if (south < -90) {
+                south = -89.999999;
             }
             if (west < -180) {
-                west = -180;
+                west = -179.999999;
             }
             if (east >= 180) {
                 east = 179.999999;
             }
-            console.log([north, east],
-                [south, west]);
             return [
                 [north, east],
                 [south, west]
             ]
         }
 
+        this.getBoundsAsGeoJSON = function(bbox) {
+            var bounds = angular.fromJson(L.rectangle(bbox).toGeoJSON().geometry);
+            bounds.crs = {type: "name", properties: {name: "EPSG:4326"}}
+            return JSON.stringify(bounds);
+        }
     }]);
 })();
