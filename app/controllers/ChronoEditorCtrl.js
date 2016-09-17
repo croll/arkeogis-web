f8407a426c08c6c94e0b4c677ac6f886fad617a3
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-09-17 19:45:53 +0200

    1chronoeditor: fix header on top, fix sidenav

--- app/controllers/ChronoEditorCtrl.js
+++ app/controllers/ChronoEditorCtrl.js
@@ -389,8 +389,31 @@
 			});
 		}
 
+		function lock_header() {
+			var fixmeTop = $('.fixme').offset().top;       // get initial position of the element
+
+			$(window).scroll(function() {                  // assign scroll event listener
+
+				var currentScroll = $(window).scrollTop(); // get current position
+
+				if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+					$('.fixme').css({                      // scroll to that element or below it
+						position: 'fixed',
+						top: '0',
+						left: '0'
+					});
+				} else {                                   // apply position: static
+					$('.fixme').css({                      // if you scroll above it
+						position: 'static'
+					});
+				}
+			});
+		}
+
+
 		function init() {
 			$scope.load();
+			lock_header();
 		}
 		init();
 
