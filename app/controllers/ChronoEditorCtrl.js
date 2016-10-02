58767f001e87f194af1a4b2c96d13fab8035efef
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-10-02 11:26:21 +0200

    add button to download a chronology csv from chronoditor

--- app/controllers/ChronoEditorCtrl.js
+++ app/controllers/ChronoEditorCtrl.js
@@ -415,6 +415,13 @@
 			});
 		}
 
+		$scope.download_csv = function() {
+			var downloadLink = angular.element('<a></a>');
+                        downloadLink.attr('href', '/api/chronologies/csv?name='+$scope.arbo.name_cur+'&isocode='+arkeoLang.getTranslationLang()+'&dl=1');
+                        downloadLink.attr('download', $scope.arbo.name_cur+'.csv');
+			downloadLink[0].click();
+		}
+
 
 		function init() {
 			$scope.load();
